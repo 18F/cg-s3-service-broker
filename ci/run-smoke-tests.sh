401b6214454a18635f7555eefc40b4b86f2a31d2
Chris Nelson <cnelson@cnelson.org>
2016-10-20 16:38:31 -0700

    Add smoke tests for public buckets (#6)

--- ci/run-smoke-tests.sh
+++ ci/run-smoke-tests.sh
@@ -1,19 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e -u
 
 cf login -a $CF_API_URL -u $CF_DEPLOY_USERNAME -p $CF_DEPLOY_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
 
-# Clean up existing app and service if present
 cf delete -f s3-smoke-tests
-cf delete-service -f s3-smoke-tests-$SERVICE_PLAN
 
-# Create service
-cf create-service s3 $SERVICE_PLAN s3-smoke-tests-$SERVICE_PLAN
+# Clean up existing app and service if present
+for SERVICE_PLAN in ${SERVICE_PLANS}; do
+	cf delete-service -f s3-smoke-tests-$SERVICE_PLAN
+	cf create-service s3 $SERVICE_PLAN s3-smoke-tests-$SERVICE_PLAN
+done
 
 # Push test app
 cf push -f s3-broker-app/ci/smoke-tests/manifest.yml -p s3-broker-app/ci/smoke-tests
 
-# Clean up app and service
-cf delete -f s3-smoke-tests
-cf delete-service -f s3-smoke-tests-$SERVICE_PLAN
+for SERVICE_PLAN in ${SERVICE_PLANS}; do
+	# Clean up app and service
+	cf delete -f s3-smoke-tests
+	cf delete-service -f s3-smoke-tests-$SERVICE_PLAN
+done
